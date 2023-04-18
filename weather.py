bbce02199ec594ee312f8cd701ca66cc32a81c6b
JesseKight <69868056+JesseKight@users.noreply.github.com>
2023-04-18 10:19:18 -0400

    Adding regx

--- weather.py
+++ weather.py
@@ -2,29 +2,32 @@ import requests
 import json
 from tkinter import *
 import apiKey
+import re
 
-#reset = False
+reset = False
 
 main = Tk()
+
 
 main.geometry("500x750")
 
 requestL = Label(main, text = "Please Enter Your Zipcode:", pady=15)
+
 requestL.pack()
 
 zipGet = Entry(main, width = 10)
 zipGet.pack()
 
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
 def apiWork():
-
+    global reset
+    
     zip = zipGet.get()
-
-    key = apiKey.serviceKey
+    if not re.match("^[0-9]{5}(?:-[0-9]{4})?$", zip):
+        exit()
+    else:
+        result = Tk()
+        result.geometry("1000x750")
+        key = apiKey.serviceKey
     
     geoCodeParse1 = "https://api.geoapify.com/v1/geocode/search?postcode="
     geoCodeParse2 = "&filter=countrycode:us,ca&format=json&apiKey="
@@ -53,24 +56,23 @@ def apiWork():
     urlApi2 = urlApi2.__add__("/forecast")
 
     response2 = requests.get(urlApi2)
-    
+
     x = 0
     while x <= 5:
-        resultsDay = Label(main, text = response2.json()['properties']['periods'][x]['name'])
+        resultsDay = Label(result, text = response2.json()['properties']['periods'][x]['name'])
         resultsDay.pack()
-        resultsTemp = Label(main, text = str(response2.json()['properties']['periods'][x]['temperature']) + 'F')
+        resultsTemp = Label(result, text = str(response2.json()['properties']['periods'][x]['temperature']) + 'F')
         resultsTemp.pack()
-        resultsWeather = Label(main, text = response2.json()['properties']['periods'][x]['detailedForecast'], pady=10, wraplength=400)
+        resultsWeather = Label(result, text = response2.json()['properties']['periods'][x]['shortForecast'], pady=10, wraplength=400)
         resultsWeather.pack()
         x = x+1
-    """
-    if reset:
-        resultsDay.destroy()
-        resultsTemp.destroy()
-        resultsWeather.destroy()
+    reset = True
+    result.mainloop()
 
-    reset = True    
-    """
+ 
+
+
+    
 
 button = Button(main, text="Enter", padx=10, pady=5, bg='white',fg='black', command=apiWork)
 button.pack()
